28de5c23d7e1f2cf1ff2d53b7ec2e5ee58448e6f
Alexander Primakov <primakovalexander07041993@gmail.com>
2020-08-09 20:32:16 +0300

    first

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,49 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.User;
+import web.service.UserService;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+    @RequestMapping(value = "/users")
+    public String printUsers(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("users", this.userService.listUsers());
+        return "users";
+    }
+    @RequestMapping(value = "/users/add", method = RequestMethod.POST)
+    public String addUser(@ModelAttribute("user") User user) {
+        if (user.getId() == 0) {
+            this.userService.addUser(user);
+        } else {
+            this.userService.updateUser(user);
+        }
+        return "redirect:/users";
+    }
+    @RequestMapping(value = "/users/delete")
+    public String deleteUser(@RequestParam("id") int id) {
+        this.userService.removeUser(id);
+        return "redirect:/users";
+    }
+
+    @RequestMapping(value = "/editUser")
+    public String editUser(@RequestParam("id") int id, Model model) {
+        model.addAttribute("user", this.userService.getUserById(id));
+        model.addAttribute("users", this.userService.listUsers());
+        return "editUser";
+    }
+}
---
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -0,0 +1,53 @@
+package web.dao;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import web.model.User;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Transactional
+    @Override
+    public void addUser(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+    @Transactional
+    @Override
+    public void updateUser(User user) {
+        sessionFactory.getCurrentSession().update(user);
+    }
+
+    @Transactional
+    @Override
+    public void removeUser(int id) {
+        User user = sessionFactory.getCurrentSession().load(User.class, id);
+        if (user != null) {
+            sessionFactory.getCurrentSession().delete(user);
+        }
+    }
+
+    @Transactional
+    @Override
+    public User getUserById(int id) {
+        return (User) sessionFactory.getCurrentSession().createQuery("from User where id = :id")
+                .setParameter("id", id).getSingleResult();
+    }
+
+    @Transactional
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+}
